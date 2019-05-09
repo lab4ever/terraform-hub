8167c57c1993a25d2d052ef30b9a5cfa6b1b3790
Ramon C3po <ramonesc3po@gmail.com>
2019-05-09 15:24:45 -0300

    Ajuste Jenkinsfile

--- backend.tf
+++ backend.tf
@@ -9,9 +9,5 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    key            = "hub/${var.region}/terraform.tfstate"
-    kms_key_id     = "arn:aws:kms:us-east-1:082715569515:key/261500aa-90e4-4769-bb62-abd5291d4859"
-    bucket         = "terraform-aws-us-east-1-backend"
-    encrypt        = "true"
   }
 }
